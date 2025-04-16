f295d937d212fa03d9a0a731095f534a6a85c6a8
SergioFloresCorrea <sergio.flores.c9029@gmail.com>
2025-04-16 15:48:12 -0500

    Started the server, no handler logic implemented

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SergioFloresCorrea/Chirpy
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+}
